85d1c59cfb47bd07517589e9886d3a35833c53c4
koynewton <nicole.newton@alumnos.udg.mx>
2019-08-11 19:30:16 -0500

    ruta heroe

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -10,14 +11,18 @@ export class HeroesComponent implements OnInit {
 
   heroes:Heroe[] = [];
 
-  constructor( private _heroesService: HeroesService) { 
+  constructor( private _heroesService: HeroesService,
+               private _router: Router) { 
     console.log("constructor");
   }
 
   ngOnInit() {
-    console.log("ngOnInit");
     this.heroes = this._heroesService.getHeroes();
-    console.log(this.heroes);
+  }
+
+  verHeroe(idx:number){
+    console.log(idx);
+    this._router.navigate(['/heroe',idx]);
   }
 
 }
